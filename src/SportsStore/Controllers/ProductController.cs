3e233f1602a0e7ec070ff30c971a4bbe6fed8f6f
Klim Kitaev <klim.kitaev@gmail.com>
2016-11-16 22:52:06 +0300

    Pagination

--- src/SportsStore/Controllers/ProductController.cs
+++ src/SportsStore/Controllers/ProductController.cs
@@ -4,18 +4,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Controllers
 {
     public class ProductController:Controller
     {
         private IProductRepository repository;
+        public int PageSize = 4;
 
         public ProductController(IProductRepository repo)
         {
             repository = repo;
         }
 
-        public ViewResult List() => View(repository.Products);
+        public ViewResult List(int page=1) => View(new ProductsListViewModel
+        {
+            Products = repository.Products.OrderBy(p => p.ProductID).Skip((page - 1) * PageSize).Take(PageSize),
+            PagingInfo=new PagingInfo
+            {
+                CurrentPage=page,
+                ItemsPerPage=PageSize,
+                TotalItems=repository.Products.Count()
+            }
+        });
     }
 }
